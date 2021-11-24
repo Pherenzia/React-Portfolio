2a36bc6dfc0edb2101fe1417e84899ed66989989
Mitchell <d.r.mitchellriley@gmail.com>
2021-11-23 22:14:58 -0800

    functional

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,7 +3,45 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      <h1>Sample Text</h1>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          ></input>
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          ></input>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Mitchell Riley</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Online Business Card</h6>
+          <p class="card-text">
+            Contact me at rileymitchellk@gmail.com
+          </p>
+          <a href="https://github.com/Pherenzia" class="card-link">
+            Github
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default function Resume() {
-    return (
-<h1>Sample text</h1>
-    );
+  return (
+    <div>
+      <h1>Resume</h1>
+      <h2>Heres a link to my most updated resume</h2>
+      <a href="https://docs.google.com/document/d/1Z8LEmeQT4wbaE3TwpbvhTI0Qd2QYWp5PzVRIWlOnrU0/edit"> Resume</a>
+    </div>
+  );
 }
